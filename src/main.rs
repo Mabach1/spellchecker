ca55b67ffae1078dbb704634a4e73d72c7ae6e27
Mabach1 <matej.baliga@gmail.com>
2024-01-19 17:26:06 +0100

    putting suggestions into a function

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ use std::fs::read_to_string;
 
 fn read_dictionary(filename: &str) -> HashSet<String> {
     let mut res = HashSet::new();
-
     for line in read_to_string(filename).unwrap().lines() {
         res.insert(line.to_string());
     }
-
     res
 }
 
@@ -33,13 +31,27 @@ fn lev(str1: &str, str2: &str) -> usize {
     .unwrap();
 }
 
-fn get_num_corrections<'a>(word: &'a str,dictionary: &'a HashSet<String>) -> Vec<(&'a String, usize)> {
+fn get_num_corrections<'a>(word: &'a str, dictionary: &'a HashSet<String>) -> Vec<(&'a String, usize)> {
     c![(s, lev(word, s)), for s in dictionary]
+}
+
+fn write_suggestions(unknown_word: &str, dictionary: &HashSet<String>) {
+    println!("\nUnknown word: \"{}\", maybe try:", unknown_word);
+
+    let mut moves = get_num_corrections(unknown_word, &dictionary);
+    moves.sort_by(|a, b| a.1.cmp(&b.1));
+
+    let suggestions = (&moves[0..=3]).to_vec();
+    let suggestions: Vec<&String> = suggestions.into_iter().map(|x| x.0).collect();
+
+    for suggestion in suggestions {
+        println!("{unknown_word} -> {suggestion}");
+    }
 }
 
 fn main() {
     let dictionary = read_dictionary("words.txt");
-    let input_string = "Helo mom, I love you!";
+    let input_string = "Helo mom, I luve you!";
 
     let filtered_input: String = input_string
         .chars()
@@ -51,20 +63,8 @@ fn main() {
 
     println!("{input_string}");
     for word in words {
-        if dictionary.contains(word) {
-            continue;
-        }
-
-        println!("\nUnknown word: \"{word}\", maybe try:");
-
-        let mut moves = get_num_corrections(word, &dictionary);
-        moves.sort_by(|a, b| a.1.cmp(&b.1));
-
-        let suggestions = (&moves[0..=5]).to_vec();
-        let suggestions: Vec<&String> = suggestions.into_iter().map(|x| { x.0 }).collect();
-        
-        for suggestion in suggestions {
-            println!("{word} -> {suggestion}");
+        if !dictionary.contains(word) {
+            write_suggestions(word, &dictionary);
         }
     }
 }
