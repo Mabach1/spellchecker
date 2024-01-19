3bdcb8421ab9c1043dbe53f6fc02c4b9f789bad8
Mabach1 <matej.baliga@gmail.com>
2024-01-19 16:46:19 +0100

    implantation of Levenshtein distance algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use cute::c;
 use std::collections::HashSet;
 use std::fs::read_to_string;
 
@@ -11,10 +12,37 @@ fn read_dictionary(filename: &str) -> HashSet<String> {
     res
 }
 
+fn lev(str1: &str, str2: &str) -> usize {
+    if str1.len() == 0 {
+        return str2.len();
+    } else if str2.len() == 0 {
+        return str1.len();
+    }
+
+    if str1.chars().nth(0).unwrap() == str2.chars().nth(0).unwrap() {
+        return lev(&str1[1..].to_string(), &str2[1..].to_string());
+    }
+
+    return 1 + [
+        lev(&str1[1..].to_string(), str2),
+        lev(str1, &str2[1..].to_string()),
+        lev(&str1[1..].to_string(), &str2[1..].to_string()),
+    ]
+    .iter()
+    .min()
+    .unwrap();
+}
+
+fn get_num_corrections<'a>(
+    word: &'a str,
+    dictionary: &'a HashSet<String>,
+) -> Vec<(&'a String, usize)> {
+    c![(s, lev(word, s)), for s in dictionary]
+}
+
 fn main() {
     let dictionary = read_dictionary("words.txt");
     let input_string = "Helo mom, I love you!";
-    //                        ^ mistake on purpose
 
     let filtered_input: String = input_string
         .chars()
@@ -26,8 +54,9 @@ fn main() {
 
     println!("{input_string}");
     for word in words {
-        if !dictionary.contains(word) {
-            println!("Unknown word: {word}");
+        if dictionary.contains(word) {
+            continue;
         }
+
     }
 }
