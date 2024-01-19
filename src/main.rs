76ea5e1c14a278138fcd141e8f493c03674c0102
Mabach1 <matej.baliga@gmail.com>
2024-01-19 15:45:09 +0100

    parsing input and checking if it's parsed words are valid

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::fs::read_to_string;
 use std::collections::HashSet;
+use std::fs::read_to_string;
 
-fn read_lines(filename: &str) -> HashSet<String> {
+fn read_dictionary(filename: &str) -> HashSet<String> {
     let mut res = HashSet::new();
 
     for line in read_to_string(filename).unwrap().lines() {
@@ -12,9 +12,22 @@ fn read_lines(filename: &str) -> HashSet<String> {
 }
 
 fn main() {
-    let dictionary = read_lines("words.txt");
+    let dictionary = read_dictionary("words.txt");
+    let input_string = "Helo mom, I love you!";
+    //                        ^ mistake on purpose
 
-    if dictionary.contains("hello") {
-        println!("containing word");
+    let filtered_input: String = input_string
+        .chars()
+        .filter(|c| c.is_alphabetic() || *c == ' ')
+        .collect();
+    let filtered_input = filtered_input.to_lowercase();
+
+    let words = filtered_input.split(" ").collect::<Vec<&str>>();
+
+    println!("{input_string}");
+    for word in words {
+        if !dictionary.contains(word) {
+            println!("Unknown word: {word}");
+        }
     }
 }
